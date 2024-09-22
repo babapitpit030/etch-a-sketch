7001e78cbc3c0ef371a767667ae79668861f9c65
babapitpit030 <jossof.h@gmail.com>
2024-09-22 16:43:29 +0200

    made the look better

--- code.js
+++ code.js
@@ -1,8 +1,10 @@
-const GRID_MEASURE = 660;
+const GRID_MEASURE = 440;
 const INITIAL_DIM = 8;
 const grids = document.querySelector(".grids");
 const clean = document.querySelector(".clean");
 const normal = document.querySelector(".normal");
+const eraser = document.querySelector(".eraser")
+const slider_text = document.querySelector(".slider-text");
 const rgb = document.querySelector(".rgb");
 let input = document.querySelector("input");
 let dim = INITIAL_DIM;
@@ -10,6 +12,13 @@ let mode = "normal";
 
 // Create the initial grid
 createGrid(INITIAL_DIM);
+
+
+// give the dimensions of the grid above the slider
+
+input.addEventListener("input", (e) => { 
+    slider_text.textContent = `${e.target.value} by ${e.target.value}`;
+})
 
 // Listen for mode change
 normal.addEventListener("click", () => {
@@ -59,10 +68,15 @@ function createGrid(dim) {
         grid.style.backgroundColor = "white";
         grid.style.width = `${GRID_MEASURE / dim}px`;
         grid.style.height = `${GRID_MEASURE / dim}px`;
-        grid.style.outline = "solid 1px black";
 
         grid.addEventListener('mouseover', () => {
             grid.style.backgroundColor = "black";
+        });
+
+        eraser.addEventListener("click", () => {
+            grid.addEventListener('mouseover', () => {
+                grid.style.backgroundColor = "white";
+            });
         });
 
         grids.appendChild(grid);
@@ -77,13 +91,18 @@ function createRGBGrids(dim) {
         grid.style.backgroundColor = "white";
         grid.style.width = `${GRID_MEASURE / dim}px`;
         grid.style.height = `${GRID_MEASURE / dim}px`;
-        grid.style.outline = "solid 1px black";
 
         grid.addEventListener('mouseover', () => {
             let x = Math.floor(Math.random() * 255);
             let y = Math.floor(Math.random() * 255);
             let z = Math.floor(Math.random() * 255);
             grid.style.backgroundColor = `rgb(${x},${y},${z})`;
+        });
+
+        eraser.addEventListener("click", () => {
+            grid.addEventListener('mouseover', () => {
+                grid.style.backgroundColor = "white";
+            });
         });
 
         grids.appendChild(grid);
